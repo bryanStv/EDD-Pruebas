3f4cb3db0b8e6c9ac076fe87db895d9ae7592814
bryanStv <bryans.oyola@gmail.com>
2023-12-21 13:32:32 +0100

    Hola Mundo 3

--- src/DiasLluviosos.java
+++ src/DiasLluviosos.java
@@ -1,11 +1,8 @@
 public class DiasLluviosos {
+    private boolean[] dias;
     public static void registrarQueLlovioElDia (int diaDeAño, boolean llovio){}
-
     public static boolean consultarSiLlovioElDia (int diaDelAño){}
-
     public static int contarDiasLluviosos (){}
-
     public static int consultarCuatrimestreMasLluvioso(){}
-
     public static int consultarPrimerDiaConLluvia(){}
 }
---
+++ test/DiasLluviososTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiasLluviososTest {
+
+    @org.junit.jupiter.api.Test
+    void registrarQueLlovioElDia() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void consultarSiLlovioElDia() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contarDiasLluviosos() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void consultarCuatrimestreMasLluvioso() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void consultarPrimerDiaConLluvia() {
+    }
+}
