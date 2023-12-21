3ef2f1a961646c3aeadc16d32291aba652532e8c
bryanStv <bryans.oyola@gmail.com>
2023-12-21 13:30:07 +0100

    Hola Mundo 2

---
+++ src/DiasLluviosos.java
@@ -0,0 +1,11 @@
+public class DiasLluviosos {
+    public static void registrarQueLlovioElDia (int diaDeAño, boolean llovio){}
+
+    public static boolean consultarSiLlovioElDia (int diaDelAño){}
+
+    public static int contarDiasLluviosos (){}
+
+    public static int consultarCuatrimestreMasLluvioso(){}
+
+    public static int consultarPrimerDiaConLluvia(){}
+}
