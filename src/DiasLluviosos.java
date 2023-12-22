e3206a9d344e3a3c968fed93c24ab288d42cd0ff
bryanStv <bryans.oyola@gmail.com>
2023-12-22 19:07:16 +0100

    Haciendo test 1.0

--- src/DiasLluviosos.java
+++ src/DiasLluviosos.java
@@ -3,10 +3,18 @@ import java.util.Arrays;
 public class DiasLluviosos {
     private static boolean[] dias = new boolean[365];
 
+    public static void setDias(boolean[] dias) {
+        DiasLluviosos.dias = dias;
+    }
+
+    public static boolean[] getDias() {
+        return dias;
+    }
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(dias));
-        registrarQueLlovioElDia(1,true);
-        System.out.println(Arrays.toString(dias));
+        //System.out.println(Arrays.toString(dias));
+        //registrarQueLlovioElDia(1,true);
+        //System.out.println(Arrays.toString(dias));
     }
     public static void registrarQueLlovioElDia (int diaDeAño, boolean llovio){
         dias[diaDeAño] = llovio;
@@ -27,14 +35,22 @@ public class DiasLluviosos {
     }
     public static int consultarCuatrimestreMasLluvioso(){
         int cuatrimestre = dias.length/4;
-        int cuatri = 1;
+        int cuatri = 1, totalRecorrido = 0, suma, resultado;
         int masLluvioso = Integer.MIN_VALUE;
-        for (int i = 0; i < dias.length; i++) {
-            for (int j = 0; j < cuatrimestre; j++) {
-                return 0;
+        while(cuatri < 5){
+            suma = 0;
+            for(int i = 0; i < cuatrimestre;i++){
+                if(dias[totalRecorrido]){
+                    suma++;
+                }
             }
+            if(suma > masLluvioso){
+                resultado = cuatri;
+                masLluvioso = suma;
+            }
+            cuatri++;
         }
-        return 0;
+        return cuatri;
     }
     public static int consultarPrimerDiaConLluvia(){
         for (int i = 0; i < dias.length; i++) {
--- test/DiasLluviososTest.java
+++ test/DiasLluviososTest.java
@@ -1,17 +1,59 @@
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiasLluviososTest {
 
+    private static void rellenarDias(){
+        //Si el indice par, no llovió(false), si es impar, llovió(true)
+        for (int i = 0; i < DiasLluviosos.getDias().length; i++) {
+            if(i % 2 != 0){
+                DiasLluviosos.getDias()[i] = true;
+            }
+        }
+    }
+
     @org.junit.jupiter.api.Test
     void registrarQueLlovioElDia() {
+        rellenarDias();
+        //System.out.println(Arrays.toString(DiasLluviosos.getDias()));
+        Assertions.assertTrue(DiasLluviosos.consultarSiLlovioElDia(1));
+        Assertions.assertFalse(DiasLluviosos.consultarSiLlovioElDia(26));
+        DiasLluviosos.registrarQueLlovioElDia(26,true);
+        Assertions.assertTrue(DiasLluviosos.consultarSiLlovioElDia(26));
+        Assertions.assertFalse(DiasLluviosos.consultarSiLlovioElDia(364));
+        DiasLluviosos.registrarQueLlovioElDia(364,true);
+        Assertions.assertTrue(DiasLluviosos.consultarSiLlovioElDia(364));
     }
 
     @org.junit.jupiter.api.Test
     void consultarSiLlovioElDia() {
+        rellenarDias();
+        Assertions.assertTrue(DiasLluviosos.consultarSiLlovioElDia(15));
+        Assertions.assertFalse(DiasLluviosos.consultarSiLlovioElDia(16));
+        Assertions.assertFalse(DiasLluviosos.consultarSiLlovioElDia(364));
+        Assertions.assertTrue(DiasLluviosos.consultarSiLlovioElDia(363));
     }
 
     @org.junit.jupiter.api.Test
     void contarDiasLluviosos() {
+        rellenarDias();
+        //La mitad entera, 182(total dias en false)
+        Assertions.assertEquals(182,DiasLluviosos.contarDiasLluviosos());
+        //Añadiendo 1 día más
+        DiasLluviosos.registrarQueLlovioElDia(0,true);
+        Assertions.assertEquals(183,DiasLluviosos.contarDiasLluviosos());
+        //Rellenando TODOS los días en true
+        for (int i = 0; i < DiasLluviosos.getDias().length; i+=2) {
+            DiasLluviosos.registrarQueLlovioElDia(i,true);
+        }
+        Assertions.assertEquals(365,DiasLluviosos.contarDiasLluviosos());
+        //Dejando el vector en falso completamente
+        boolean[] diasVacio = new boolean[365];
+        DiasLluviosos.setDias(diasVacio);
+        Assertions.assertEquals(0,DiasLluviosos.contarDiasLluviosos());
     }
 
     @org.junit.jupiter.api.Test
@@ -20,5 +62,16 @@ class DiasLluviososTest {
 
     @org.junit.jupiter.api.Test
     void consultarPrimerDiaConLluvia() {
+        //Vector vacío, ningún día llovió, devuelve -1
+        DiasLluviosos.setDias(new boolean[365]);
+        Assertions.assertEquals(-1,DiasLluviosos.consultarPrimerDiaConLluvia());
+        //Rellenando vector
+        DiasLluviosos.registrarQueLlovioElDia(16,true); //Primer día
+        DiasLluviosos.registrarQueLlovioElDia(20,true);
+        DiasLluviosos.registrarQueLlovioElDia(80,true);
+        DiasLluviosos.registrarQueLlovioElDia(104,true);
+        Assertions.assertEquals(16,DiasLluviosos.consultarPrimerDiaConLluvia());
+        rellenarDias(); //Con el vector completamente relleno con días impares además de los anteriores pares
+        Assertions.assertEquals(1,DiasLluviosos.consultarPrimerDiaConLluvia());
     }
 }
